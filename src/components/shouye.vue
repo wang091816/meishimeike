f6a784ac8c80f40e672a8c187f89611f7a71d282
wangbo <13100196713@163.com>
2020-06-30 16:13:05 +0800

    刚刚把路由创建完成

---
+++ src/components/shouye.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+     <p>我是首页标签</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,132 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+const Holloworld = resolve => require(["../components/HelloWorld"], resolve);
+const Shouye = resolve => require(["../components/shouye"], resolve);
+const Cepingjilu = resolve => require(["../components/cepingjilu"], resolve);
+const Cuotilianxi = resolve => require(["../components/cuotilianxi"], resolve);
+const Dengluzhuce = resolve => require(["../components/dengluzhuce"], resolve);
+const Fengzhenmokao = resolve => require(["../components/fengzhenmokao"], resolve);
+const Jingpinkecheng = resolve => require(["../components/jingpinkecheng"], resolve);
+const Lanxinfeng = resolve => require(["../components/lanxinfeng"], resolve);
+const Mingshizhenrong = resolve => require(["../components/mingshizhenrong"], resolve);
+const Kaodianlianxi = resolve => require(["../components/kaodianlianxi"], resolve);
+const Kecheng = resolve => require(["../components/kecheng"], resolve);
+const Taojuanlianxi = resolve => require(["../components/taojuanlianxi"], resolve);
+const Teseke = resolve => require(["../components/teseke"], resolve);
+const Wode = resolve => require(["../components/wode"], resolve);
+const Xitishouceng = resolve => require(["../components/xitishouceng"], resolve);
+const Xuexirili = resolve => require(["../components/xuexirili"], resolve);
+const Yuduiyifudao = resolve => require(["../components/yiduiyifudao"], resolve);
+const Yuekejilu = resolve => require(["../components/yuekejilu"], resolve);
+const Lianxi = resolve => require(["../components/lianxi"], resolve);
+const Kechengxingqing = resolve => require(["../components/kechengxingqing"], resolve);
 Vue.use(VueRouter)
+
+
+
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name:"Holloworld",
+    component:Holloworld
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Shouye',
+    name:"Shouye",
+    component:Shouye
+  },
+  {
+    path: '/Cepingjilu',
+    name:"Cepingjilu",
+    component:Cepingjilu
+  },
+  {
+    path: '/Cuotilianxi',
+    name:"Cuotilianxi",
+    component:Cuotilianxi
+  },
+  {
+    path: '/Dengluzhuce',
+    name:"Dengluzhuce",
+    component:Dengluzhuce
+  },
+  {
+    path: '/Fengzhenmokao',
+    name:"Fengzhenmokao",
+    component:Fengzhenmokao
+  },
+  {
+    path: '/Jingpinkecheng',
+    name:"Jingpinkecheng",
+    component:Jingpinkecheng
+  },
+  {
+    path: '/Lanxinfeng',
+    name:"Lanxinfeng",
+    component:Lanxinfeng
+  },
+  {
+    path: '/Mingshizhenrong',
+    name:"Mingshizhenrong",
+    component:Mingshizhenrong
+  },
+  {
+    path: '/Kaodianlianxi',
+    name:"Kaodianlianxi",
+    component:Kaodianlianxi
+  },
+  {
+    path: '/Kecheng',
+    name:"Kecheng",
+    component:Kecheng
+  },
+  {
+    path: '/Taojuanlianxi',
+    name:"Taojuanlianxi",
+    component:Taojuanlianxi
+  },
+  {
+    path: '/Teseke',
+    name:"Teseke",
+    component:Teseke
+  },
+  {
+    path: '/Wode',
+    name:"Wode",
+    component:Wode
+  },
+  {
+    path: '/Xitishouceng',
+    name:"Xitishouceng",
+    component:Xitishouceng
+  },
+  {
+    path: '/Xuexirili',
+    name:"Xuexirili",
+    component:Xuexirili
+  },
+  {
+    path: '/Yuduiyifudao',
+    name:"Yuduiyifudao",
+    component:Yuduiyifudao
+  },
+  {
+    path: '/Yuekejilu',
+    name:"Yuekejilu",
+    component:Yuekejilu
+  },
+  {
+    path: '/Lianxi',
+    name:"Lianxi",
+    component:Lianxi
+  },
+  {
+    path: '/Kechengxingqing',
+    name:"Kechengxingqing",
+    component:Kechengxingqing
+  },
 ]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
